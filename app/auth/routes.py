c43834b93afd5e2ce087d0ee2c58ea88eac705dd
AlejandroD999 <alejandrodgonzales923@gmail.com>
2025-12-13 10:47:24 -0600

    Styled home_layout to make overall website take a starry night on desert style

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,42 +1,11 @@
 from flask import Blueprint, flash, render_template, redirect, request, session, url_for
 import sqlite3
-from .utils import check_valid_password
+from .utils import check_valid_password, is_logged_in
 from app.database.utils import get_db, create_users_table, get_cursor
 from app.extensions import bcrypt
 
 auth = Blueprint('auth', __name__, template_folder='templates', static_folder='static')
 
-@auth.route("/login", methods=["GET", "POST"])
-def login():
-
-    if request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("password")
-
-        with get_db() as conn: 
-            # Get users When username, and password match
-            create_users_table(conn)
-
-            with get_cursor(conn) as cur:
-                cur.execute("SELECT * FROM users WHERE username = ?", (username,))
-                user_info = cur.fetchone()
-
-
-        # If password does not match or user doesn't exist
-        if not user_info or not bcrypt.check_password_hash(user_info["password"], password):
-            flash("Invalid username or password")
-            return redirect(url_for("auth.login"))
-
-
-        session["username"] = user_info["username"]
-        session["logged_in"] = True
-        session.permanent = True
-
-        return redirect(url_for("home.home"))
-
-    else:
-        return render_template("login.html")
-    
 @auth.route("/signup", methods=["GET", "POST"])
 def signup():
     """ Get username and password then insert into database """
@@ -74,3 +43,42 @@ def signup():
         return redirect(url_for("auth.login"))
 
     return render_template("signup.html")
+
+@auth.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        with get_db() as conn: 
+            # Get users When username, and password match
+            create_users_table(conn)
+
+            with get_cursor(conn) as cur:
+                cur.execute("SELECT * FROM users WHERE username = ?", (username,))
+                user_info = cur.fetchone()
+
+
+        # If password does not match or user doesn't exist
+        if not user_info or not bcrypt.check_password_hash(user_info["password"], password):
+            flash("Invalid username or password")
+            return redirect(url_for("auth.login"))
+
+
+        session["username"] = user_info["username"]
+        session["logged_in"] = True
+        session.permanent = True
+
+        return redirect(url_for("home.home"))
+
+    else:
+        return render_template("login.html")
+    
+@auth.route("/logout")
+def logout():
+    if is_logged_in():
+        session.pop(session['username'], None)
+        
+        return redirect(url_for("auth.login"))
+    return redirect(url_for("auth.login"))
